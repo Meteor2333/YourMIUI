774c78515fd0588cead5133d3562d302ed400268
Meteor2333 <Meteor23333@outlook.com>
2026-04-28 20:01:44 +0800

    fix: 推送漏掉的一些内容

--- core/src/main/java/cc/meteormc/yourmiui/core/Scope.kt
+++ core/src/main/java/cc/meteormc/yourmiui/core/Scope.kt
@@ -3,6 +3,8 @@ package cc.meteormc.yourmiui.core
 interface Scope {
     fun getNameRes(): Int?
 
+    fun isRestartable(): Boolean
+
     fun getPackages(): Array<String>
 
     fun getFeatures(): Iterable<Any>
