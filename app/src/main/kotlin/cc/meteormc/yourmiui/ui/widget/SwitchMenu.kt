e0e91bdec3b5a934f680914388eb8ac0b533076a
Meteor2333 <Meteor23333@outlook.com>
2026-05-07 18:23:47 +0800

    refactor: 提取部分ui逻辑为组件

---
+++ app/src/main/kotlin/cc/meteormc/yourmiui/ui/widget/SwitchMenu.kt
@@ -0,0 +1,55 @@
+package cc.meteormc.yourmiui.ui.widget
+
+import android.content.Context
+import android.view.View
+import androidx.appcompat.widget.PopupMenu
+import cc.meteormc.yourmiui.R
+
+private const val ENABLE_BOTTON_ID = 0x0
+private const val DISABLE_BOTTON_ID = 0x1
+
+class SwitchMenu(
+    context: Context,
+    anchor: View,
+    private val initial: Boolean
+) : PopupMenu(context, anchor) {
+    private var changeListener: (selected: Boolean) -> Unit = { }
+
+    override fun show() {
+        val enableBotton = menu.add(0, ENABLE_BOTTON_ID, 0, R.string.popup_enable)
+        val disableBotton = menu.add(0, DISABLE_BOTTON_ID, 0, R.string.popup_disable)
+        if (initial) {
+            enableBotton.isCheckable = true
+            enableBotton.isChecked = true
+        } else {
+            disableBotton.isCheckable = true
+            disableBotton.isChecked = true
+        }
+
+        setOnMenuItemClickListener {
+            when (it.itemId) {
+                ENABLE_BOTTON_ID -> {
+                    enableBotton.isCheckable = true
+                    enableBotton.isChecked = true
+                    disableBotton.isCheckable = false
+                    changeListener(true)
+                }
+                DISABLE_BOTTON_ID -> {
+                    enableBotton.isCheckable = false
+                    disableBotton.isCheckable = true
+                    disableBotton.isChecked = true
+                    changeListener(false)
+                }
+            }
+
+            return@setOnMenuItemClickListener true
+        }
+
+        super.show()
+    }
+
+    fun setChangeListener(listener: (selected: Boolean) -> Unit): SwitchMenu {
+        changeListener = listener
+        return this
+    }
+}
