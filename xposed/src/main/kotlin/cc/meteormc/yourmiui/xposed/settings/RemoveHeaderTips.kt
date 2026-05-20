da55d64e3639da60700cbe35c6b88b87b992a0f4
Meteor2333 <Meteor23333@outlook.com>
2026-05-20 19:30:21 +0800

    refactor: 适配

--- xposed/src/main/kotlin/cc/meteormc/yourmiui/xposed/settings/RemoveHeaderTips.kt
+++ xposed/src/main/kotlin/cc/meteormc/yourmiui/xposed/settings/RemoveHeaderTips.kt
@@ -1,16 +1,10 @@
-package cc.meteormc.yourmiui.xposed.settings.feature
+package cc.meteormc.yourmiui.xposed.settings
 
 import cc.meteormc.yourmiui.common.Feature
-import cc.meteormc.yourmiui.xposed.R
 import cc.meteormc.yourmiui.xposed.operator
 
-object RemoveHeaderTips : Feature(
-    "remove_header_tips",
-    nameRes = R.string.feature_settings_remove_header_tips_name,
-    descriptionRes = R.string.feature_settings_remove_header_tips_description,
-    testEnvironmentRes = R.string.feature_settings_remove_header_tips_test_environment
-) {
-    override fun onLoadPackage() {
+object RemoveHeaderTips : Feature() {
+    override fun onLoadPackage(packageName: String) {
         operator("com.android.settings.SettingsFragment") {
             // modifier: private | signature: updateTips(ZLjava/lang/String;IIILandroid/view/View$OnClickListener;)V
             method("updateTips")?.hookDoNothing()
