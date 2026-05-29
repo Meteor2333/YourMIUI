d5fed2b33f187d2c9ca6756cac71211a6b44762f
Meteor2333 <Meteor23333@outlook.com>
2026-05-30 01:56:51 +0800

    refactor: 删除冗余内容

--- common/src/main/kotlin/cc/meteormc/yourmiui/common/bridge/Bridge.kt
+++ common/src/main/kotlin/cc/meteormc/yourmiui/common/bridge/Bridge.kt
@@ -1,12 +1,9 @@
 package cc.meteormc.yourmiui.common.bridge
 
-import cc.meteormc.yourmiui.common.Scope
-
 object Bridge {
     internal const val RESPONSE_ACTION = "cc.meteormc.yourmiui.ACTION_RESPONSE"
     internal const val REQUIRED_PERMISSION = "cc.meteormc.yourmiui.permission.USE_BRIDGE"
 
-    val GET_SCOPES_CHANNEL = Channel<Unit, ArrayList<Scope>>("cc.meteormc.yourmiui.ACTION_GET_SCOPES")
     val RESTART_SCOPE_CHANNEL = Channel<Unit, Unit>("cc.meteormc.yourmiui.ACTION_RESTART_SCOPE")
 
     var apiName: String? = null
