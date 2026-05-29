104c5cd46b92d11becf46707d31ccd68da7439e9
Meteor2333 <Meteor23333@outlook.com>
2026-05-29 21:35:55 +0800

    feature: 添加功能分类

---
+++ api/src/main/kotlin/cc/meteormc/yourmiui/api/Category.kt
@@ -0,0 +1,17 @@
+package cc.meteormc.yourmiui.api
+
+enum class Category {
+    SYSTEM,
+    UI,
+    DESKTOP,
+    MARTET,
+    MIUI_PLUS,
+    MMS,
+    NFC,
+    NOTIFICATION,
+    PACKAGE_INSTALLER,
+    PORTAL,
+    SECURITY_CENTER,
+    SETTINGS,
+    SUPER_WALLPAPER
+}
--- api/src/main/kotlin/cc/meteormc/yourmiui/api/annotation/FeatureRegister.kt
+++ api/src/main/kotlin/cc/meteormc/yourmiui/api/annotation/FeatureRegister.kt
@@ -1,9 +1,11 @@
 package cc.meteormc.yourmiui.api.annotation
 
+import cc.meteormc.yourmiui.api.Category
+
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class FeatureRegister(
-    val category: String,
+    val category: Category,
     val name: String,
     val description: String,
     val warning: String = "",
