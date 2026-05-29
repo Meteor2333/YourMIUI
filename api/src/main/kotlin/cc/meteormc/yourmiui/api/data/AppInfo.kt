aacff0c4d66afa8fc7d159893c9dff266257a4d0
Meteor2333 <Meteor23333@outlook.com>
2026-05-30 01:11:53 +0800

    fix: 修复报错问题

--- api/src/main/kotlin/cc/meteormc/yourmiui/api/data/AppInfo.kt
+++ api/src/main/kotlin/cc/meteormc/yourmiui/api/data/AppInfo.kt
@@ -1,16 +1,11 @@
 package cc.meteormc.yourmiui.api.data
 
-import android.graphics.Bitmap
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
-@Parcelize
 data class AppInfo(
     val packageName: String,
-    val label: String,
-    val icon: Bitmap
-) : Serializable, Parcelable {
+    val label: String
+) : Serializable {
     override fun equals(other: Any?) = other is AppInfo && packageName == other.packageName
 
     override fun hashCode() = packageName.hashCode()
