08da4e3c4c1035afe9babc3308182ad0c6754a1d
Meteor2333 <Meteor23333@outlook.com>
2026-05-30 02:28:36 +0800

    refactor: 使XposedEntry适配新机制

--- xposed/src/main/kotlin/cc/meteormc/yourmiui/xposed/XposedEntry.kt
+++ xposed/src/main/kotlin/cc/meteormc/yourmiui/xposed/XposedEntry.kt
@@ -4,97 +4,76 @@ import android.app.Application
 import android.content.Context
 import android.os.Process
 import android.util.Log
-import cc.meteormc.yourmiui.common.Feature
-import cc.meteormc.yourmiui.common.Option
-import cc.meteormc.yourmiui.common.Scope
+import cc.meteormc.yourmiui.FeatureRegistry
+import cc.meteormc.yourmiui.api.FeatureHooker
+import cc.meteormc.yourmiui.api.annotation.EntryClass
 import cc.meteormc.yourmiui.common.bridge.Bridge
 import cc.meteormc.yourmiui.common.bridge.Host
 import cc.meteormc.yourmiui.common.util.ClassUtil
-import cc.meteormc.yourmiui.xposed.android.Android
-import cc.meteormc.yourmiui.xposed.contentextension.ContentExtension
-import cc.meteormc.yourmiui.xposed.home.Home
-import cc.meteormc.yourmiui.xposed.market.Market
-import cc.meteormc.yourmiui.xposed.mirror.Mirror
-import cc.meteormc.yourmiui.xposed.mms.MMS
-import cc.meteormc.yourmiui.xposed.nfc.NFC
-import cc.meteormc.yourmiui.xposed.notification.Notification
-import cc.meteormc.yourmiui.xposed.packageinstaller.PackageInstaller
-import cc.meteormc.yourmiui.xposed.securitycenter.SecurityCenter
-import cc.meteormc.yourmiui.xposed.settings.Settings
-import cc.meteormc.yourmiui.xposed.superwallpaper.SuperWallpaper
-import cc.meteormc.yourmiui.xposed.systemadsolution.SystemAdSolution
-import cc.meteormc.yourmiui.xposed.systemui.SystemUI
-import de.robv.android.xposed.IXposedHookInitPackageResources
+import cc.meteormc.yourmiui.common.util.PrefsUtil
+import cc.meteormc.yourmiui.common.util.SingletonUtil
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XSharedPreferences
 import de.robv.android.xposed.XposedBridge
-import de.robv.android.xposed.callbacks.XC_InitPackageResources
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.lang.reflect.Modifier
 
-class XposedEntry : IXposedHookInitPackageResources, IXposedHookLoadPackage {
-    private lateinit var hostBridge: Host
-    private val scopes by lazy {
-        listOf(
-            Android,
-            ContentExtension,
-            Home,
-            Market,
-            Mirror,
-            MMS,
-            NFC,
-            Notification,
-            PackageInstaller,
-            SecurityCenter,
-            Settings,
-            SuperWallpaper,
-            SystemAdSolution,
-            SystemUI
-        )
+@EntryClass
+class XposedEntry : IXposedHookLoadPackage {
+    companion object {
+        lateinit var INSTANCE: XposedEntry
+            private set
     }
+
+    init {
+        INSTANCE = this
+    }
+
+    lateinit var hostBridge: Host
+    lateinit var classLoader: ClassLoader
+
     private val prefs by lazy {
-        XSharedPreferences("cc.meteormc.yourmiui", Feature.PREFERENCES_NAME).apply {
+        XSharedPreferences("cc.meteormc.yourmiui", PrefsUtil.SHARED_PREFERENCES_NAME).apply {
             makeWorldReadable()
             reload()
         }
     }
 
-    override fun handleInitPackageResources(resparam: XC_InitPackageResources.InitPackageResourcesParam) {
-        initFeatures(resparam.packageName) { scope, feature ->
-            feature.resources = resparam.res
-
-            runCatching {
-                feature.onInitResources()
-            }.onFailure { exception ->
-                XposedBridge.log(
-                    "[YourMIUI] Failed to " +
-                            "initialize resources for feature '${feature.id}' " +
-                            "in scope '${scope.id}':\n" +
-                            Log.getStackTraceString(exception)
-                )
-            }
-        }
-    }
-
     @Suppress("UNCHECKED_CAST")
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        initFeatures(lpparam.packageName) { scope, feature ->
-            feature.classLoader = lpparam.classLoader
+        classLoader = lpparam.classLoader
 
+        val packageName = lpparam.packageName
+        initFeatures(lpparam.packageName) {
             runCatching {
-                feature.getOptions().forEach { option ->
-                    val key = Feature.optionKeyOf(feature.key, option.key)
-                    val value = prefs.getString(key, null)?.let { preference ->
-                        option.type.deserializer(preference)
-                    } ?: option.defaultValue
-                    (option as Option<Any>).onValueInit(value)
+                operator(it.javaClass) {
+                    for (field in declaredFields()) {
+                        // todo
+                        val value = null
+
+                        val instance = if (Modifier.isStatic(field.modifiers())) {
+                            null
+                        } else {
+                            SingletonUtil.getInstance(delegate) as? FeatureHooker?
+                        }
+                        field[instance] = value
+                    }
                 }
 
-                feature.onLoadPackage()
+//                feature.getOptions().forEach { option ->
+//                    val key = Feature.optionKeyOf(feature.key, option.key)
+//                    val value = prefs.getString(key, null)?.let { preference ->
+//                        option.type.deserializer(preference)
+//                    } ?: option.defaultValue
+//                    (option as Option<Any>).onValueInit(value)
+//                }
+
+                it.hook(packageName)
             }.onFailure { exception ->
                 XposedBridge.log(
                     "[YourMIUI] Failed to " +
-                            "initialize feature '${feature.id}' " +
-                            "in scope '${scope.id}':\n" +
+                            "initialize feature '${it.javaClass.simpleName}' " +
+                            "in package '${packageName}':\n" +
                             Log.getStackTraceString(exception)
                 )
             }
@@ -103,16 +82,14 @@ class XposedEntry : IXposedHookInitPackageResources, IXposedHookLoadPackage {
 
         operator(Application::class.java) {
             method("attach")?.hookAfter {
-                initHostBridge(lpparam.classLoader, it.instance())
+                initHostBridge(it.instance())
             }
         }
     }
 
-    private fun initHostBridge(classLoader: ClassLoader, context: Context) {
+    private fun initHostBridge(context: Context) {
         hostBridge = Host(context)
-        hostBridge.register(Bridge.GET_SCOPES_CHANNEL) {
-            scopes.toCollection(ArrayList())
-        }.register(Bridge.RESTART_SCOPE_CHANNEL) {
+        hostBridge.register(Bridge.RESTART_SCOPE_CHANNEL) {
             Thread {
                 Thread.sleep(300)
                 Process.killProcess(Process.myPid())
@@ -132,12 +109,9 @@ class XposedEntry : IXposedHookInitPackageResources, IXposedHookLoadPackage {
         }
     }
 
-    private fun initFeatures(packageName: String, initializer: (scope: Scope, feature: Feature) -> Unit) {
-        val scope = this.scopes.firstOrNull {
-            it.packages.contains(packageName)
-        } ?: return
-        scope.getFeatures()
-            .filter { prefs.getBoolean(Feature.enabledKeyOf(it.key), false) }
-            .forEach { initializer(scope, it) }
+    private fun initFeatures(packageName: String, initializer: (hooker: FeatureHooker) -> Unit) {
+        FeatureRegistry.features[packageName]
+            ?.filter { prefs.getBoolean(PrefsUtil.getFeatureEnabledKey(it.javaClass.simpleName), false) }
+            ?.forEach { initializer(it) }
     }
 }
