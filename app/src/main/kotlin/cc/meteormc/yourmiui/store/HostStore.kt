006fea811a4e5bb9e40195c6eba44b0e2e5884a8
Meteor2333 <Meteor23333@outlook.com>
2026-05-30 19:04:18 +0800

    feature: 完善store的适配

--- app/src/main/kotlin/cc/meteormc/yourmiui/store/HostStore.kt
+++ app/src/main/kotlin/cc/meteormc/yourmiui/store/HostStore.kt
@@ -2,15 +2,19 @@ package cc.meteormc.yourmiui.store
 
 import androidx.lifecycle.MutableLiveData
 import cc.meteormc.yourmiui.BuildConfig
+import cc.meteormc.yourmiui.api.Category
+import cc.meteormc.yourmiui.api.FeatureHooker
 import cc.meteormc.yourmiui.api.data.FeatureInfo
 import cc.meteormc.yourmiui.common.bridge.Bridge
 import cc.meteormc.yourmiui.common.util.ClassUtil
 import cc.meteormc.yourmiui.common.util.SingletonUtil
+import java.util.EnumMap
 
 object HostStore {
     val apiName = MutableLiveData("Unknown")
     val apiVersion = MutableLiveData(-1)
     val isActivated = MutableLiveData(false)
+    val features = MutableLiveData<EnumMap<Category, List<FeatureInfo>>>()
 
     fun init() {
         val apiName = Bridge.apiName
@@ -23,24 +27,20 @@ object HostStore {
         this.apiName.value = apiName
         this.apiVersion.value = apiVersion
         this.isActivated.value = true
-
-        // todo
-        ClassUtil.getClass(
+        this.features.value = ClassUtil.getClass(
             javaClass.classLoader!!,
             "${BuildConfig.APPLICATION_ID}.FeatureRegistry",
             true
         )?.run {
-            val instance = SingletonUtil.getInstance(this) ?: return@run null
-
+            val instance = SingletonUtil.getInstance(this)
             @Suppress("UNCHECKED_CAST")
-            val feature =
-                getDeclaredMethod("getFeatures").invoke(instance) as? Map<String, List<Any>>
-                    ?: return@run null
-            return@run feature.values
-                .flatten()
-                .distinct()
-                .map { FeatureInfo.fromHooker(it) }
-                .groupBy { it.category }
-        } ?: emptyMap()
+            getDeclaredMethod("getFeatures").invoke(instance) as? Map<String, List<FeatureHooker>>
+        }.orEmpty()
+            .values
+            .flatten()
+            .distinct()
+            .map { FeatureInfo.fromHooker(it) }
+            .groupByTo(EnumMap(Category::class.java)) { it.category }
+            .run { EnumMap(this) }
     }
 }
