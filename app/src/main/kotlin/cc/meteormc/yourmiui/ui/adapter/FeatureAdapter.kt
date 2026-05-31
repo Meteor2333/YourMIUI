f0bb433e02303aaaa245e6ab8737b7e0ef2be967
Meteor2333 <Meteor23333@outlook.com>
2026-05-31 17:08:17 +0800

    wip: 解除注释
    我也不知道为什么要解除这个注释但是反正看见了还是想解除虽然我也不知道为什么要解除

--- app/src/main/kotlin/cc/meteormc/yourmiui/ui/adapter/FeatureAdapter.kt
+++ app/src/main/kotlin/cc/meteormc/yourmiui/ui/adapter/FeatureAdapter.kt
@@ -1,13 +1,18 @@
 package cc.meteormc.yourmiui.ui.adapter
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import cc.meteormc.yourmiui.R
 import cc.meteormc.yourmiui.YourMIUI
 import cc.meteormc.yourmiui.api.data.FeatureInfo
+import cc.meteormc.yourmiui.common.bridge.Bridge
+import cc.meteormc.yourmiui.common.bridge.ResponseCallback
 import cc.meteormc.yourmiui.common.prefs.SharedPreferences
 import cc.meteormc.yourmiui.databinding.ItemFeatureBinding
 import cc.meteormc.yourmiui.helper.ResourceParser
+import cc.meteormc.yourmiui.ui.widget.BottomSheet
+import java.util.concurrent.atomic.AtomicInteger
 
 class FeatureAdapter(
     features: List<FeatureInfo>
@@ -67,52 +72,52 @@ class FeatureAdapter(
         }
 
         // todo: 未来会做成长按功能弹出菜单 然后能够执行如 重启所需作用域 等功能
-//        private fun executeForceStop(feature: FeatureInfo) {
-//            val scopes = feature.scopes
-//            val context = itemView.context
-//
-//            fun request(packageName: String) {
-//                YourMIUI.get().moduleBridge.request(
-//                    Bridge.RESTART_SCOPE_CHANNEL,
-//                    packageName,
-//                    object : ResponseCallback<Unit> {
-//                        private var successd = false
-//                        private val count = AtomicInteger(scopes.size)
-//
-//                        override fun onSuccess(data: Unit) {
-//                            successd = true
-//                            onCallback()
-//                        }
-//
-//                        override fun onFailure() {
-//                            onCallback()
-//                        }
-//
-//                        private fun onCallback() {
-//                            if (count.decrementAndGet() > 0) return
-//                            Toast.makeText(
-//                                context,
-//                                if (successd) R.string.restart_scope_success else R.string.restart_scope_failure,
-//                                if (successd) Toast.LENGTH_SHORT else Toast.LENGTH_LONG
-//                            ).show()
-//                        }
-//                    }
-//                )
-//            }
-//
-//            BottomSheet.Builder(context)
-//                .setTitle(R.string.restart_scope_title)
-//                .setContent(
-//                    R.string.restart_scope_content,
-//                    scopes.joinToString("\n")
-//                )
-//                .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
-//                .setPositiveButton(android.R.string.ok) { dialog, _ ->
-//                    dialog.dismiss()
-//                    scopes.forEach { request(it) }
-//                }
-//                .build()
-//                .show()
-//        }
+        private fun executeForceStop(feature: FeatureInfo) {
+            val scopes = feature.scopes
+            val context = itemView.context
+
+            fun request(packageName: String) {
+                YourMIUI.get().moduleBridge.request(
+                    Bridge.RESTART_SCOPE_CHANNEL,
+                    packageName,
+                    object : ResponseCallback<Unit> {
+                        private var successd = false
+                        private val count = AtomicInteger(scopes.size)
+
+                        override fun onSuccess(data: Unit) {
+                            successd = true
+                            onCallback()
+                        }
+
+                        override fun onFailure() {
+                            onCallback()
+                        }
+
+                        private fun onCallback() {
+                            if (count.decrementAndGet() > 0) return
+                            Toast.makeText(
+                                context,
+                                if (successd) R.string.restart_scope_success else R.string.restart_scope_failure,
+                                if (successd) Toast.LENGTH_SHORT else Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                )
+            }
+
+            BottomSheet.Builder(context)
+                .setTitle(R.string.restart_scope_title)
+                .setContent(
+                    R.string.restart_scope_content,
+                    scopes.joinToString("\n")
+                )
+                .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
+                .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                    dialog.dismiss()
+                    scopes.forEach { request(it) }
+                }
+                .build()
+                .show()
+        }
     }
 }
