300b6a45d0565d01683733e7cd10c8393f4c873b
Meteor2333 <Meteor23333@outlook.com>
2026-05-31 16:59:38 +0800

    refactor: 删除parcelize依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.agp.application) apply false
     alias(libs.plugins.agp.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
 }
 
 val minSdkVersion by extra(24)
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.agp.library)
-    alias(libs.plugins.kotlin.parcelize)
 }
 
 val projectNamespace: String by rootProject.extra
