2ed4be658059c410bdd7504cedeba242624dd2a4
Meteor2333 <Meteor23333@outlook.com>
2026-06-06 14:53:08 +0800

    feat: 剥离CategoryResProvider来提供分类图标和名称

--- app/src/main/kotlin/cc/meteormc/yourmiui/ui/adapter/CategoryAdapter.kt
+++ app/src/main/kotlin/cc/meteormc/yourmiui/ui/adapter/CategoryAdapter.kt
@@ -1,15 +1,12 @@
 package cc.meteormc.yourmiui.ui.adapter
 
-import android.content.pm.PackageManager
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.navigation.findNavController
 import cc.meteormc.yourmiui.R
 import cc.meteormc.yourmiui.api.Category
 import cc.meteormc.yourmiui.databinding.ItemCategoryBinding
+import cc.meteormc.yourmiui.helper.CategoryResProvider.getDiaplayName
+import cc.meteormc.yourmiui.helper.CategoryResProvider.getIcon
 
 class CategoryAdapter(
     scopes: List<Category>
@@ -28,73 +25,17 @@ class CategoryAdapter(
         binding.root
     ) {
         override fun onBind(item: Category) {
-            val name = getCategoryName(item)
-            val icon = getCategoryIcon(item)
+            val icon = item.getIcon()
+            val displayName = item.getDiaplayName()
 
             itemView.setOnClickListener {
                 val bundle = Bundle()
-                bundle.putString("title", name)
                 bundle.putInt("category", item.ordinal)
                 it.findNavController().navigate(R.id.action_home_to_category, bundle)
             }
 
-            binding.categoryName.text = name
+            binding.categoryName.text = displayName
             binding.categoryIcon.setImageDrawable(icon)
-        }
-
-        private fun getCategoryName(category: Category): String {
-            fun provideByPkg(packageName: String): String {
-                val pm = itemView.context.packageManager
-                val info = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
-                return pm.getApplicationLabel(info).toString()
-            }
-
-            fun provideByRes(@StringRes resId: Int): String {
-                return itemView.context.getString(resId)
-            }
-
-            return when (category) {
-                Category.SYSTEM -> provideByRes(R.string.category_system)
-                Category.UI -> provideByRes(R.string.category_ui)
-                Category.DESKTOP -> provideByRes(R.string.category_desktop)
-                Category.MARTET -> provideByPkg("com.xiaomi.market")
-                Category.MIUI_PLUS -> provideByRes(R.string.category_miuiplus)
-                Category.MMS -> provideByPkg("com.android.mms")
-                Category.NFC -> provideByRes(R.string.category_nfc)
-                Category.NOTIFICATION -> provideByRes(R.string.category_notification)
-                Category.PACKAGE_INSTALLER -> provideByRes(R.string.category_packageinstaller)
-                Category.PORTAL -> provideByPkg("com.miui.contentextension")
-                Category.SECURITY_CENTER -> provideByPkg("com.miui.securitycenter")
-                Category.SETTINGS -> provideByPkg("com.android.settings")
-                Category.WALLPAPER -> provideByPkg("com.miui.miwallpaper")
-            }
-        }
-
-        private fun getCategoryIcon(category: Category): Drawable {
-            fun provideByPkg(packageName: String): Drawable {
-                val pm = itemView.context.packageManager
-                return pm.getApplicationIcon(packageName)
-            }
-
-            fun provideByRes(@DrawableRes resId: Int): Drawable {
-                return AppCompatResources.getDrawable(itemView.context, resId)!!
-            }
-
-            return when (category) {
-                Category.SYSTEM -> provideByPkg("android")
-                Category.UI -> provideByPkg("com.android.systemui")
-                Category.DESKTOP -> provideByPkg("com.miui.home")
-                Category.MARTET -> provideByPkg("com.xiaomi.market")
-                Category.MIUI_PLUS -> provideByPkg("com.xiaomi.mirror")
-                Category.MMS -> provideByPkg("com.android.mms")
-                Category.NFC -> provideByPkg("com.android.nfc")
-                Category.NOTIFICATION -> provideByPkg("com.miui.notification")
-                Category.PACKAGE_INSTALLER -> provideByPkg("com.miui.packageinstaller")
-                Category.PORTAL -> provideByPkg("com.miui.contentextension")
-                Category.SECURITY_CENTER -> provideByPkg("com.miui.securitycenter")
-                Category.SETTINGS -> provideByPkg("com.android.settings")
-                Category.WALLPAPER -> provideByPkg("com.miui.miwallpaper")
-            }
         }
     }
 }
--- app/src/main/kotlin/cc/meteormc/yourmiui/ui/fragment/CategoryFragment.kt
+++ app/src/main/kotlin/cc/meteormc/yourmiui/ui/fragment/CategoryFragment.kt
@@ -6,27 +6,26 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import cc.meteormc.yourmiui.api.Category
 import cc.meteormc.yourmiui.databinding.FragmentCategoryBinding
+import cc.meteormc.yourmiui.helper.CategoryResProvider.getDiaplayName
 import cc.meteormc.yourmiui.store.HostStore
 import cc.meteormc.yourmiui.ui.adapter.FeatureAdapter
 
 class CategoryFragment : BaseFragment<FragmentCategoryBinding>({ inflater, container ->
     FragmentCategoryBinding.inflate(inflater, container, false)
 }) {
-    private val title: String?
-        get() = arguments?.getString("title")
     private val category: Category?
         get() = arguments?.getInt("category", -1)
             ?.takeIf { it >= 0 }
             ?.let { Category.entries[it] }
 
     override fun onCreate(): View {
-        if (title == null || category == null) {
+        if (category == null) {
             findNavController().navigateUp()
             return binding.root
         }
 
         val categoryToolbar = binding.categoryToolbar
-        categoryToolbar.title = title
+        categoryToolbar.title = category!!.getDiaplayName()
         categoryToolbar.setNavigationOnClickListener {
             it.findNavController().navigateUp()
         }
