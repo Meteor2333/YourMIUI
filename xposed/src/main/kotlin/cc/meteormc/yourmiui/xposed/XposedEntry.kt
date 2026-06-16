eadc15b52ae0195c1003da2ea11c694515a3d729
Meteor2333 <Meteor23333@outlook.com>
2026-06-16 11:14:43 +0800

    refactor: 重构XposedEntry

--- xposed/src/main/kotlin/cc/meteormc/yourmiui/xposed/XposedEntry.kt
+++ xposed/src/main/kotlin/cc/meteormc/yourmiui/xposed/XposedEntry.kt
@@ -1,10 +1,10 @@
 package cc.meteormc.yourmiui.xposed
 
 import android.app.Application
-import android.content.Context
 import android.os.Process
 import android.util.Log
 import cc.meteormc.yourmiui.FeatureRegistry
+import cc.meteormc.yourmiui.api.FeatureHooker
 import cc.meteormc.yourmiui.api.OptionType
 import cc.meteormc.yourmiui.api.annotation.EntryClass
 import cc.meteormc.yourmiui.api.data.FeatureInfo
@@ -23,7 +23,6 @@ import java.lang.reflect.Modifier
 
 @EntryClass
 class XposedEntry : IXposedHookLoadPackage {
-    lateinit var hostBridge: Host
     private val prefs by lazy {
         SharedPreferences(
             XSharedPreferences(
@@ -39,75 +38,102 @@ class XposedEntry : IXposedHookLoadPackage {
     @Suppress("UNCHECKED_CAST")
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         val classLoader = lpparam.classLoader
+        val packageName = lpparam.packageName
         val context = HookContext(
             lpparam.appInfo?.uid ?: 0,
-            lpparam.packageName,
+            packageName,
             lpparam.processName,
-            lpparam.classLoader
+            classLoader
         )
 
-        val packageName = lpparam.packageName
-        FeatureRegistry.features[packageName]
-            ?.map { FeatureInfo.fromHooker(it) }
-            ?.forEach {
-                val prefs = prefs.getFeature(it)
-                if (!prefs.enabled) return@forEach
+        val hookers: List<FeatureHooker>? = FeatureRegistry.features[packageName]
+        val features = hookers?.map { FeatureInfo.fromHooker(it) } ?: emptyList()
+        features.forEach { syncFeature(context, it) }
 
-                runCatching {
-                    it.options.forEach { option ->
-                        val source = option.source
-                        val type = option.type
-                        val value = when (type) {
-                            is OptionType.App -> prefs.getOption(option.key, type) ?: type.defaultPackages
-                            is OptionType.List -> prefs.getOption(option.key, type) ?: type.defaultOptions
-                            is OptionType.Switch -> prefs.getOption(option.key, type) ?: type.defaultValue
-                            is OptionType.Text -> prefs.getOption(option.key, type) ?: type.defaultText
-                        }
+        Application::class.reflect.method("attach")?.hookAfter { param ->
+            Host(param.instance()).register(Bridge.FORCE_STOP_CHANNEL) {
+                Thread {
+                    Thread.sleep(300)
+                    Process.killProcess(Process.myPid())
+                }.start()
+            }.attach()
 
-                        if (!PrimitiveUtil.canAssign(value.javaClass, source.type)) {
-                            throw IllegalStateException(
-                                "Option '${option.key}' in feature '${it.javaClass.simpleName}' " +
-                                        "expects a value of type ${source.type}, but got ${value.javaClass}"
-                            )
-                        }
-
-                        val instance = if (Modifier.isStatic(source.modifiers)) null
-                        else SingletonUtil.getInstance(it.source)
-                        source[instance] = value
-                    }
-
-                    it.hooker.hook(context)
-                }.onFailure { exception ->
-                    XposedBridge.log(
-                        "[YourMIUI] Failed to " +
-                                "initialize feature '${it.javaClass.simpleName}' " +
-                                "in package '${packageName}':\n" +
-                                Log.getStackTraceString(exception)
-                    )
-                }
+            ClassUtil.getClass(classLoader, Bridge::class.java.name, true)?.reflect {
+                val apiName = Reflect(XposedBridge::class.java).run {
+                    field("TAG")?.get(null)
+                } ?: "Unknown"
+                val apiVersion = XposedBridge.getXposedVersion()
+                field("apiName")?.set(null, apiName)
+                field("apiVersion")?.set(null, apiVersion)
             }
-
-        Application::class.reflect.method("attach")?.hookAfter {
-            initHostBridge(it.instance(), classLoader)
         }
     }
 
-    private fun initHostBridge(context: Context, classLoader: ClassLoader) {
-        hostBridge = Host(context)
-        hostBridge.register(Bridge.FORCE_STOP_CHANNEL) {
-            Thread {
-                Thread.sleep(300)
-                Process.killProcess(Process.myPid())
-            }.start()
-        }.attach()
+    private fun syncFeature(context: HookContext, feature: FeatureInfo) {
+        val prefs = prefs.getFeature(feature)
+        if (prefs.enabled) {
+            updateOptions(feature, prefs)
+            hookFeature(context, feature)
+        } else {
+            unhookFeature(context, feature)
+        }
+    }
 
-        ClassUtil.getClass(classLoader, Bridge::class.java.name, true)?.reflect {
-            val apiName = Reflect(XposedBridge::class.java).run {
-                field("TAG")?.get(null)
-            } ?: "Unknown"
-            val apiVersion = XposedBridge.getXposedVersion()
-            field("apiName")?.set(null, apiName)
-            field("apiVersion")?.set(null, apiVersion)
+    private fun hookFeature(context: HookContext, feature: FeatureInfo) {
+        val hooker = feature.hooker
+        if (context.isHooked(hooker)) return
+
+        runCatching {
+            hooker.hook(context)
+            context.hooked.add(hooker)
+        }.onFailure { exception ->
+            XposedBridge.log(
+                "[YourMIUI] Failed to " +
+                        "hook feature '${feature.javaClass.simpleName}' " +
+                        "in package '${feature.category}':\n" +
+                        Log.getStackTraceString(exception)
+            )
+        }
+    }
+
+    private fun unhookFeature(context: HookContext, feature: FeatureInfo) {
+        val hooker = feature.hooker
+        if (!context.isHooked(hooker)) return
+
+        runCatching {
+            hooker.unhook()
+            context.hooked.remove(hooker)
+        }.onFailure {
+            XposedBridge.log(
+                "[YourMIUI] Failed to " +
+                        "unhook feature '${feature.javaClass.simpleName}' " +
+                        "in package '${feature.category}':\n" +
+                        Log.getStackTraceString(it)
+            )
+        }
+    }
+
+    private fun updateOptions(feature: FeatureInfo, preferences: SharedPreferences.Feature) {
+        feature.options.forEach { option ->
+            val source = option.source
+            val type = option.type
+            val value = when (type) {
+                is OptionType.App -> preferences.getOption(option.key, type) ?: type.defaultPackages
+                is OptionType.List -> preferences.getOption(option.key, type) ?: type.defaultOptions
+                is OptionType.Switch -> preferences.getOption(option.key, type) ?: type.defaultValue
+                is OptionType.Text -> preferences.getOption(option.key, type) ?: type.defaultText
+            }
+
+            if (!PrimitiveUtil.canAssign(value.javaClass, source.type)) {
+                throw IllegalStateException(
+                    "Option '${option.key}' in feature '${feature.javaClass.simpleName}' " +
+                            "expects a value of type ${source.type}, but got ${value.javaClass}"
+                )
+            }
+
+            val instance = if (Modifier.isStatic(source.modifiers)) null
+            else SingletonUtil.getInstance(feature.source)
+            source[instance] = value
         }
     }
 }
